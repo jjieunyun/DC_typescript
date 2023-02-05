6f4b76fa13b19a7edcfc1084733c9db53792bd99
jieun <lesson0186@gmail.com>
2023-02-05 21:22:13 +0900

    oop개념

---
+++ oop/3-7-composition.ts
@@ -0,0 +1,162 @@
+{
+  /**
+   *💡 상속의 문제점 : 상속의 깊이가 깊어지면 복잡성이 증가함
+   * 부모클래스에서 수정하면 자식도 영향을 받는다.
+   * 타입스크립트는 : 한가지 이상의 부모클래스를 상속할 수없다.!
+   *
+   * ❤️‍🔥 composition사용 : 필요한 것들을 조립해서 다시 만듬
+   * 상속보다 composition을 더 선호하는 것이 좋다.
+   *
+   *❗️ 클래스들 사이에는 interface를 통해 상호작용 해야한다.
+   */
+
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface 0MilkFrother {
+    makeMilk(cup: CoffeeCup): CoffeeCup;
+  }
+
+  interface SugarSource {
+    addSugar(cup: CoffeeCup): CoffeeCup;
+  }
+
+  //클래스를 상속하는 것이 아니라 interface를 implenent함
+  class CheapMilkSteamer implements MilkFrother {
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      console.log(`Steaming some milk🥛...`);
+      return {
+        ...cup,
+        hasMilk: true,
+      };
+    }
+  }
+
+  class FancyMilkSteamer implements MilkFrother {
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      console.log(`Fancy!!!! Steaming some milk🥛...`);
+      return {
+        ...cup,
+        hasMilk: true,
+      };
+    }
+  }
+
+  //설탕 제조기
+  class AutomaticSugarMixer implements SugarSource {
+    addSugar(cuppa: CoffeeCup): CoffeeCup {
+      console.log(`Adding sugar...`);
+      return {
+        ...cuppa,
+        hasSugar: true,
+      };
+    }
+  }
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  //커피머신 클래스
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
+    private coffeeBeans: number = 0; // instance (object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log("cleaning the machine...🧼");
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log("heating up... 🔥");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots... ☕️`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+    private steamMilk(): void {
+      console.log("Steaming some milk... 🥛");
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
+    }
+  }
+
+  class SweetCoffeeMaker extends CoffeeMachine {
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return {
+        ...coffee,
+        hasSugar: true,
+      };
+    }
+  }
+
+  //💡composition으로 만든 클래스
+  class SweetCaffeLatteMachine extends CoffeeMachine {
+    constructor(
+      beans: number,
+      private sugar: SugarSource,
+      private milk: MilkFrother
+    ) {
+      super(beans);
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      const milkCoffee = this.milk.makeMilk(coffee);
+      return this.sugar.addSugar(milkCoffee);
+    }
+  }
+  const machine = new SweetCaffeLatteMachine(
+    32,
+    new AutomaticSugarMixer(),
+    new FancyMilkSteamer()
+  );
+  machine.makeCoffee(2);
+}
